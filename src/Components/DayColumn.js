fa96d187f30879385bf9818e81063aecaa82192e
Kosta Dinkov <k.v.dinkov@gmail.com>
2022-11-26 10:32:48 +0200

    add: day Column date is now a link to the specific date

--- src/Components/DayColumn.js
+++ src/Components/DayColumn.js
@@ -2,9 +2,9 @@ import React from "react";
 import styles from "./DayColumn.module.css";
 import OrderCard from "./OrderCard";
 import moment from "moment/moment";
+import { Link } from "react-router-dom";
 
 const DayColumn = ({ data }) => {
-  
   const date = moment(data[0].pickupDate);
 
   data = data.sort((a, b) => {
@@ -18,7 +18,10 @@ const DayColumn = ({ data }) => {
   return (
     <section>
       <header>
-        <div><span className = {styles.date}>{date.date()}</span> <span>{date.format("MMMM yyyy")}</span></div>
+        <Link to={`/orders/forDay/${date.format()}`}>
+          <span className={styles.date}>{date.date()}</span>{" "}
+          <span className={styles.monthYear}>{date.format("MMMM yyyy")}</span>
+        </Link>
       </header>
       <div className={styles.orderList}>
         {data.map((order) => (

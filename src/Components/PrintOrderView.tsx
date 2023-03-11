188448cc9ed09aa85733c0fdd5017b17c0c8c42b
Kosta Dinkov <k.v.dinkov@gmail.com>
2023-03-11 15:38:30 +0200

    add: include photo price to cake

--- src/Components/PrintOrderView.tsx
+++ src/Components/PrintOrderView.tsx
@@ -1,25 +1,33 @@
-import React, {useContext} from "react";
+import React, { useContext } from "react";
 import styles from "./PrintOrderView.module.css";
 import OrderDTO from "../Types/OrderDTO";
 import { format } from "date-fns";
 import { bg, is } from "date-fns/locale";
 import { OrdersService } from "../API/ordersApi";
-import { useLoaderData} from "react-router-dom";
+import { useLoaderData } from "react-router-dom";
 import AppContext from "../appContext";
 import ProductDTO from "../Types/ProductDTO";
 import OrderItemDTO from "../Types/OrderItemDTO";
 
-export async function loader ({params}:{params:{id:number}}){
-    let id = params.id;
-    let order = await OrdersService.GetOrderAsync(id);
-    console.log(order);
-    return order;
+let PhotoPrice: number;
+
+export async function loader({ params }: { params: { id: number } }) {
+  let id = params.id;
+  let order = await OrdersService.GetOrderAsync(id);
+  console.log(order);
+  return order;
 }
 
 export default function PrintOrderView() {
+  const { products } = useContext(AppContext);
+  const foto = products.find((p) => p.id === 350);
+  if (foto) {
+    PhotoPrice = foto.priceDrebno;
+  } else {
+    throw new Error("Не може да бъде намерен продукт Фотокартина (id");
+  }
+  const order = useLoaderData() as OrderDTO;
 
-    const order = useLoaderData() as OrderDTO;
-   
   return (
     <div className={styles.pageView}>
       <OrderForPrint order={order} />
@@ -30,13 +38,12 @@ export default function PrintOrderView() {
 }
 
 function OrderForPrint({ order }: { order: OrderDTO }) {
-
-    let isSpecialPrice = false;
-    const {clients} = useContext(AppContext);
-    const client = clients.find(c=>c.id === order.clientId);
-    if(client){
-        isSpecialPrice = client.isSpecialPrice;
-    }
+  let isSpecialPrice = false;
+  const { clients } = useContext(AppContext);
+  const client = clients.find((c) => c.id === order.clientId);
+  if (client) {
+    isSpecialPrice = client.isSpecialPrice;
+  }
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -109,25 +116,35 @@ function OrderForPrint({ order }: { order: OrderDTO }) {
               </span>
             </div>
             <div className={styles.productPrice}>
-              <span>{toBGN(i.productAmount * getProductPrice(i,isSpecialPrice))}</span>
+              <span>
+                {toBGN(i.productAmount * getProductPrice(i, isSpecialPrice))}
+              </span>
             </div>
           </div>
         ))}
       </div>
 
       <div className={styles.footer}>
-        <Footer order={order} isSpecialPrice={isSpecialPrice}/>
+        <Footer order={order} isSpecialPrice={isSpecialPrice} />
       </div>
     </div>
   );
 }
 
-function Footer({ order, isSpecialPrice }: { order: OrderDTO, isSpecialPrice:boolean }) {
+function Footer({
+  order,
+  isSpecialPrice,
+}: {
+  order: OrderDTO;
+  isSpecialPrice: boolean;
+}) {
   if (order.isPaid) {
     return (
       <div className={styles.footerRow}>
         <span className={styles.footerInfo}>ПЛАТЕНА</span>
-        <span className={styles.footerTotal}>Сума: {getOrderPrice(order, isSpecialPrice)}</span>
+        <span className={styles.footerTotal}>
+          Сума: {getOrderPrice(order, isSpecialPrice)}
+        </span>
       </div>
     );
   } else if (order.advancePaiment > 0) {
@@ -153,9 +170,9 @@ function Footer({ order, isSpecialPrice }: { order: OrderDTO, isSpecialPrice:boo
   }
 }
 
-function getOrderPrice(order: OrderDTO, isSpecialPrice:boolean): number {
+function getOrderPrice(order: OrderDTO, isSpecialPrice: boolean): number {
   let price = 0;
-  
+
   for (let item of order.orderItems) {
     price += getProductPrice(item, isSpecialPrice) * item.productAmount;
     //TODO calculate foto price
@@ -170,11 +187,16 @@ function toBGN(amount: number): string {
   return amount.toLocaleString("bg-BG", { style: "currency", currency: "BGN" });
 }
 
-function getProductPrice(orderItem:OrderItemDTO, isSpecialPrice: boolean ){
-    if(isSpecialPrice){
-        return Math.round((orderItem.product.priceDrebno / 1.5)*100)/100;
-    }
-    else{
-        return orderItem.product.priceDrebno;
-    }
+function getProductPrice(orderItem: OrderItemDTO, isSpecialPrice: boolean) {
+  let price = 0;
+  if(orderItem.cakeFoto?.trim()!== ""){
+    price += PhotoPrice;
+  }
+  if (isSpecialPrice) {
+    price += Math.round((orderItem.product.priceDrebno / 1.5) * 100) / 100;
+  } else {
+    price += orderItem.product.priceDrebno;
+  }
+
+  return price;
 }

245f33d050728cfabe7c008f3861f5e1b16e2244
Kosta Dinkov <k.v.dinkov@gmail.com>
2023-03-21 16:38:16 +0200

    test: add new test case for special price function

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import { render, screen } from '@testing-library/react';
 
 import {getSpecialPrice} from './system/utils.ts';
 
-let cases = [[10, 7.33], [14.4, 10.56], [17.7, 12.98],[15.2, 11.15],[28,20.53],[30,22]]
+let cases = [[10, 7.33], [14.4, 10.56], [17.7, 12.98],[15.2, 11.15],[28,20.53],[30,22],[0,0]]
 test.each(cases)('For priceDrebno = %p, special price  should be %p', () => {
   
   let discount = 25;

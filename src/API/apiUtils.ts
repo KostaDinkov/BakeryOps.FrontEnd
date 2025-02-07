ab0cd1973f919d373d380ba25a51081c13a27bbd
Kosta Dinkov <k.v.dinkov@gmail.com>
2025-02-07 18:08:27 +0200

    feat: Enhance API response handling with detailed error formatting and improved notifications

--- src/API/apiUtils.ts
+++ src/API/apiUtils.ts
@@ -1,23 +1,40 @@
-import {useNotifications} from '@toolpad/core/useNotifications';
-type FetchCall = () => Promise<any>;
+import { useNotifications } from "@toolpad/core/useNotifications";
+import { FetchResponse } from "openapi-fetch";
+import { ErrorResponse, ResponseObjectMap } from "openapi-typescript-helpers";
+type FetchCall = () => Promise<FetchResponse<any, any, any>>;
 
-
+type Error = {
+  errors: { [key: string]: string[] };
+  status: number;
+  title: string;
+};
 export async function handleApiResponse(fetchCall: FetchCall) {
-    
-    const fetchResponse = await fetchCall();
-    if (fetchResponse.error) {
-      throw fetchResponse.error;
-    }
-    return fetchResponse.data;
+  const fetchResponse = await fetchCall();
+  if (fetchResponse.error) {
+    throw fetchResponse.error as ErrorResponse<ResponseObjectMap<any>, any>;
+  }
+  return fetchResponse.data;
 }
 
 export function useHandleApiResponse() {
   const notifications = useNotifications();
+
   return async function (fetchCall: FetchCall) {
     try {
       return await handleApiResponse(fetchCall);
     } catch (error) {
-      notifications.show(`Грешка при запис: ${error}`, { severity: 'error' });
+      notifications.show(`Грешка при запис: Статус ${error.status}`, { severity: "error" });
+      throw new Error(`Грешка при запис \n${formatResponseErrors(error as Error)}`);
     }
   };
+}
+
+
+function formatResponseErrors(error: Error) {
+  const lines: string[] = [
+    error.title,
+    `Status: ${error.status}`,
+    ...Object.keys(error.errors).map(key => `${key}: ${error.errors[key]?.join(', ')}`),
+  ];
+  return lines.join("\n");
 }

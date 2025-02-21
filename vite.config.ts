9d2306b97f4e4a19e839c61a85e8c77777bf7ce2
Kosta Dinkov <k.v.dinkov@gmail.com>
2025-02-21 07:56:07 +0200

    feat: Update environment configuration, enhance Dockerfile, and improve client form handling; add nginx configuration for CORS support

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,27 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig, loadEnv } from 'vite';
 import {UserConfig} from "vite";
 import react from "@vitejs/plugin-react";
 
-
-export default defineConfig({
-  server: {
-    host: true,
-    port: 5173,
-    allowedHosts:["localhost","192.168.1.195"]
-  },
+export default defineConfig(({ mode }) => {
+  // Load all environment variables based on the current mode
+  const env = loadEnv(mode, process.cwd())
+  // Merge them into process.env for use in the config
+  Object.assign(process.env, env)
   
-  build: {
-    outDir: "build",
-  },
-  plugins: [react()],
-  test: {
-    environment: 'node',
-    include: ['tests/**/*.test.ts']
-  }
-} satisfies UserConfig);
+  return {
+    server: {
+      host: true,
+      port: 5173,
+      allowedHosts:["localhost","192.168.1.195"]
+    },
+    
+    build: {
+      outDir: "build",
+    },
+    plugins: [react()],
+    test: {
+      environment: 'node',
+      include: ['tests/**/*.test.ts']
+    }
+  } satisfies UserConfig;
+});
